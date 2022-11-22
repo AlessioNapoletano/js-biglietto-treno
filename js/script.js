b93ff78f0124d8e7612f69fcac5bd4fe8c88589a
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-11-22 01:27:20 +0100

    add Comment, other functions and output

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@
     - Stampare il prezzo.
 */
 
-//Richiesta dati
+//data request
 let kilometres = prompt("Inserisci il numero di chilometri: ");
 
 let agePassenger = prompt("Inserisci l'età del passeggero");
@@ -19,7 +19,7 @@ console.log("il numero di chilometri del tuo viaggio: " + " " + kilometres);
 
 console.log("l'età del passeggero è: " + " " + agePassenger);
 
-//Casting delle due variabili
+//variable castings
 kilometres = parseInt(kilometres);
 
 agePassenger = parseInt(agePassenger);
@@ -27,18 +27,24 @@ agePassenger = parseInt(agePassenger);
 
 let price = kilometres * 0.21;
 
-
+//Console test
 console.log("il prezzo è: " + " " + price + "\u20ac");
 
 
 if(agePassenger < 18){
     price = price - (price * 0.20);
+    document.getElementById("minor_adult").innerHTML = "Il cliente è minorenne, usufruirà quindi dello sconto del 20%";
 }else if(agePassenger >= 65)  {
     price = price - (price * 0.40);
+    document.getElementById("minor_adult").innerHTML = "Il cliente è over 65, usufruirà quindi dello sconto del 40%";
+} else{
+    document.getElementById("minor_adult").innerHTML = "Per il cliente non è prevista una scontistica";
 }
 
+//only 2 decimal places
 const formattedPrice = price.toFixed(2);
 
+//output result
 document.getElementById('kilometres').innerHTML = kilometres + " Km";
 document.getElementById('age_passenger').innerHTML = agePassenger + " Anni";
 document.getElementById('final_price').innerHTML = formattedPrice + "\u20ac";
